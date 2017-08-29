6f010d2a3d666a2fe5fac6b483dc352f15532327
fede <federico.maidan@gaudi.com.ar>
2017-08-29 12:00:58 -0300

    mejoras proceso ebay

--- src/AppBundle/Command/ActualizarPublicacionesEbayCommand.php
+++ src/AppBundle/Command/ActualizarPublicacionesEbayCommand.php
@@ -5,8 +5,8 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use AppBundle\Utils\Meli\Meli;
 use AppBundle\Entity\BusquedaEbay;
+use Symfony\Component\Debug\Exception;
 
 class ActualizarPublicacionesEbayCommand extends ContainerAwareCommand
 {
@@ -22,11 +22,24 @@ class ActualizarPublicacionesEbayCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        
-        $busquedas = $this->getContainer()->get('doctrine')->getManager()->getRepository(BusquedaEbay::ORM_ENTITY)->findAll();
-        foreach ($busquedas as $key => $busqueda) {
-            $this->getContainer()->get('ebay_service')->actualizarPublicaciones($busqueda);
+        try {
+            $busquedas = $this->getContainer()->get('doctrine')->getManager()->getRepository(BusquedaEbay::ORM_ENTITY)->findAll();
+            foreach ($busquedas as $key => $busqueda) {
+                $this->getContainer()->get('ebay_service')->actualizarPublicaciones($busqueda);
+            }
         }
+        catch(Exception\OutOfMemoryException $e) {
+                $this->cambiarEstadoBusqueda($busqueda, $this->getEstadoActual() . " - Error por falla en memoria");
+        }
+        catch(Exception\FatalErrorException $e) {
+                $this->cambiarEstadoBusqueda($busqueda, $this->getEstadoActual() . " - Error por FatalError - ". $e->message);
+        }
+
         
+    }
+
+    private function cambiarEstadoBusqueda($busqueda, $texto) {
+        $busqueda->setEstadoActual(date('Y-m-d h:i:s')." - ".$texto);
+        $this->em->flush();
     }
 }
