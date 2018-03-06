344282a58c61c34f764ec5b5ed55ce7c25f22a53
fede <federico.maidan@gaudi.com.ar>
2018-03-06 12:30:21 -0300

    init command publi ebay

--- src/AppBundle/Command/MercadoLibre/PublicacionMasivaEbayMLCommand.php
+++ src/AppBundle/Command/MercadoLibre/PublicacionMasivaEbayMLCommand.php
@@ -58,7 +58,7 @@ class PublicacionMasivaEbayMLCommand extends ContainerAwareCommand
     	
     	$cuenta = $this->getContainer()->get('doctrine')->getManager()->getRepository(Cuenta::class)->findOneById($id_cuenta);
 
-		$token = "APP_USR-3659532861516182-030605-ba83eef67e7539b017338ac213aeb16f__N_A__-73818038";
+		$token = "APP_USR-3659532861516182-030611-de00299c1779ebce56c8d4b22a6aedcd__A_L__-73818038";
 		$id_ebay  = $data[0];
     	$rentabilidad = $data[1];
     	$shipping = $data[2];
