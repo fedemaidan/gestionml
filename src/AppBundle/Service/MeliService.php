e51321fe3b3d0a050b024af02b4df139fde0235a
fede <federico.maidan@gaudi.com.ar>
2018-03-06 07:20:28 -0300

    init command publi ebay

--- src/AppBundle/Service/MeliService.php
+++ src/AppBundle/Service/MeliService.php
@@ -189,6 +189,7 @@ class MeliService
     private function armarTitulo($texto) {
         $sufijo = "*a Pedido 30dias!";
         $texto = substr($texto, 0, 60 - strlen($sufijo));
+        var_dump($texto." ".$sufijo);die;
         return $texto." ".$sufijo;
     }
 
