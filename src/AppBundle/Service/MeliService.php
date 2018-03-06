79789ce6c988eb3c413dee3e0124b4763a684700
fede <federico.maidan@gaudi.com.ar>
2018-03-06 11:29:25 -0300

    init command publi ebay

--- src/AppBundle/Service/MeliService.php
+++ src/AppBundle/Service/MeliService.php
@@ -203,9 +203,11 @@ class MeliService
     private function predecirCategoria($publicacion) {
         $meli = new Meli("","");
         $titulo = str_replace("&", " ", $publicacion->getTitulo());
+        $titulo = str_replace(" ", "\"", $titulo);
+        var_dump($titulo);
         $url = "sites/MLA/category_predictor/predict?title=".$titulo."&seller_id=".$publicacion->getCuenta()->getIdMl()."&price=".$publicacion->getPrecioCompra();
         $url = str_replace(" ", "%", $url);
-        $url = str_replace(" ", "\"", $url);
+
         $datos = $meli->get($url);
         var_dump($datos);die;
         return $datos["body"]->id;
