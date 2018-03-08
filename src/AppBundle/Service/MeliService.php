b30bd60e957699cda18e2b4df5b1212450c8b2a1
fede <federico.maidan@gaudi.com.ar>
2018-03-08 13:49:54 -0300

    probando no cargar repetidos

--- src/AppBundle/Service/MeliService.php
+++ src/AppBundle/Service/MeliService.php
@@ -141,7 +141,8 @@ class MeliService
     }
 
     public function replicarPublicacionEbayEnMl($ebay, $cuentaML, $token, $rentabilidad = 4, $shipping = 10) {
-        $publicacionExiste = $this->em->getRepository(PublicacionPropia::class)->findOneByPublicacionEbayId($ebay->getId());
+        $publicacionExiste = $this->em->getRepository(PublicacionPropia::class)->findOneBy([ "publicacion_ebay_id" => $ebay->getId()]);
+        
         if ($publicacionExiste) {
             var_dump("Ya esta cargada ".$ebay->getId());
             return;
