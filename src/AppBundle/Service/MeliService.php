d084c02428d3d1dbf0cc459847e0fee858eba3d6
fede <federico.maidan@gaudi.com.ar>
2018-03-13 21:42:37 -0300

    probando put

--- src/AppBundle/Service/MeliService.php
+++ src/AppBundle/Service/MeliService.php
@@ -222,7 +222,7 @@ class MeliService
         $datos = $meli->put("items/".$publicacionPropia->getIdMl(), $body, [ "access_token" => $token ]);
         var_dump($datos);die;
         if ($datos["httpCode"] != 200 ) {
-            throw new \Exception("message", 1);
+            throw new \Exception($datos["message"], 1);
         }
 
         return $datos;
